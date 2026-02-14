401917d8d10457085cb5d2cb091246366b3ffc12
Serhiy Ponomarenko <ponomarenko404@gmail.com>
2026-02-14 11:28:32 +0200

    add task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const buttonChange = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+buttonChange.addEventListener("click", function () {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  color.textContent = color;  
+});
+
+
